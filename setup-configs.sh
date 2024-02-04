fa55579e3725255329e0cb2be19fde6159faada8
YohanGH <YohanGH@proton.me>
2024-02-04 16:46:02 +0100

    Modification

--- setup-configs.sh
+++ setup-configs.sh
@@ -6,11 +6,25 @@
 #    By: YohanGH <YohanGH@proton.me>                    //    ''     Code      #
 #                                                      (|     | )              #
 #    Created: 2024/02/04 15:04:28 by YohanGH           '__   _/_               #
-#    Updated: 2024/02/04 15:18:53 by YohanGH          (___)=(___)              #
+#    Updated: 2024/02/04 16:45:35 by YohanGH          (___)=(___)              #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
+
+echo "Début de la configuration de l'environnement..."
+
+# Fonction pour afficher une barre de progression simple
+progress_bar() {
+    echo -n "Progression : ["
+    for i in {1..50}; do
+        sleep 0.02
+        echo -n "#"
+    done
+    echo "] Terminé!"
+}
+
+echo "-----"
 
 # Fonction pour vérifier la réussite du clonage/mise à jour
 check_success() {
@@ -22,24 +36,35 @@ check_success() {
     fi
 }
 
+echo "-----"
+
 # Fonction pour cloner ou mettre à jour un dépôt
 clone_or_update_repo() {
     local repo_url=$1
     local target_dir=$2
     local repo_name=$(basename $repo_url)
 
-    echo "Traitement de $repo_name..."
+    echo -e "\nTraitement de $repo_name..."
+    progress_bar
 
     if [ -d "$target_dir/.git" ]; then
-        echo "Mise à jour du dépôt $repo_name..."
+        echo -e "\nMise à jour du dépôt $repo_name..."
         git -C "$target_dir" pull
         check_success
     else
-        echo "Clonage du dépôt $repo_name vers $target_dir..."
+        echo -e "\nClonage du dépôt $repo_name vers $target_dir..."
         git clone $repo_url "$target_dir"
         check_success
     fi
 }
+
+echo "-----"
+
+# Installation d'Oh My Zsh
+echo -e "\nInstallation d'Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+progress_bar
+check_success
 
 # Configuration pour zshrc et Vim
 CONFIG_ZSHRC_URL="https://github.com/YohanGH/Configuration_zshrc"
@@ -63,16 +88,19 @@ clone_or_update_repo https://github.com/zsh-users/zsh-autosuggestions $ZSH_AUTOS
 clone_or_update_repo https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_SYNTAX_HIGHLIGHTING_DIR
 
 # Téléchargez et installez Powerlevel10k
+echo -e "\nInstallation de Powerlevel10k..."
 if [ ! -d "$P10K_DIR/.git" ]; then
-    echo "Installation de Powerlevel10k..."
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $P10K_DIR
+    progress_bar
     check_success
 else
     echo "Powerlevel10k est déjà installé."
 fi
 
+echo "-----"
+
 # Déplacez les fichiers de configuration, s'ils existent
-echo "Déplacement des fichiers de configuration..."
+echo -e "\nDéplacement des fichiers de configuration..."
 if [ -f "$CONFIG_ZSHRC_DIR/zshrc" ]; then
     cp "$CONFIG_ZSHRC_DIR/zshrc" "$HOME/.zshrc"
 else
@@ -80,10 +108,75 @@ else
 fi
 
 if [ -f "$CONFIG_VIM_DIR/vimrc" ]; then
-    cp "$CONFIG_VIM_DIR/vimrc" "$HOME/.vimrc"
+    cp "$CONFIG_VIM_DIR/.vimrc" "$HOME/.vimrc"
 else
     echo "Le fichier vimrc est introuvable, vérifiez le dépôt $CONFIG_VIM_URL."
 fi
 
-echo "Configuration terminée."
+echo "---------------------------------------------------------------------"
 
+# Emplacement de base pour la création de l'arborescence GTD
+BASE_DIR="$HOME/Documents/GTD"
+
+# Liste des chemins de dossiers à créer
+declare -a FOLDERS=(
+    "1_In-Box"
+    "Activable-NON/Incubation"
+    "Activable-NON/Références/Contacts/Professionnelle"
+    "Activable-NON/Références/Contacts/Personnel"
+    "Activable-NON/Références/DataBase"
+    "Activable-NON/Références/Documents/Textuel"
+    "Activable-NON/Références/Documents/images"
+    "Activable-NON/Références/Documents/PDF"
+    "Activable-NON/Références/Documents/Autres"
+    "Activable-NON/Références/Livres"
+    "Activable-OUI/EnAttente/Professionnel"
+    "Activable-OUI/EnAttente/Personnel"
+    "Activable-OUI/Projets/Professionnel/DevOps"
+    "Activable-OUI/Projets/Professionnel/Docker"
+    "Activable-OUI/Projets/Professionnel/Testing"
+    "Activable-OUI/Projets/Professionnel/GitHub"
+    "Activable-OUI/Projets/Professionnel/Administration"
+    "Activable-OUI/Projets/Personnel/DevOps"
+    "Activable-OUI/Projets/Personnel/Docker"
+    "Activable-OUI/Projets/Personnel/Testing"
+    "Activable-OUI/Projets/Personnel/GitHub"
+    "Activable-OUI/Projets/Personnel/Administration"
+    "Activable-OUI/Sous-Traitez/En Attente"
+    "Liste-Contrôle"
+    "Idées-Réflexions/Post-it"
+    "Archives/Note-Quotidienne"
+    "Archives/Professionnel"
+    "Archives/Agenda"
+    "Archives/Personnel"
+    "Template"
+)
+
+echo "-----"
+
+# Fonction pour afficher l'arborescence en cours de création
+print_tree() {
+    local folder=$1
+    local prefix="|---"
+    # Remplace les slashs par des barres verticales pour simuler une structure d'arbre
+    local tree_path=$(echo "$folder" | sed 's:/:/|---:g')
+    echo -e "${prefix}${tree_path}"
+}
+
+echo "Création de l'arborescence GTD dans $BASE_DIR :"
+
+echo "-----"
+
+# Création de l'arborescence
+for folder in "${FOLDERS[@]}"; do
+    mkdir -p "$BASE_DIR/$folder"
+    print_tree "$folder"
+done
+
+echo "-----"
+
+echo "L'arborescence GTD a été créée avec succès dans $BASE_DIR."
+
+echo "---------------------------------------------------------------------"
+
+echo -e "\nConfiguration terminée."
